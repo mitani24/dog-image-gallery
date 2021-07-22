2a548c6831ed632c4ca776b140d617676bec0688
mitani24 <h.mitani@ashita-team.com>
2021-07-22 18:05:47 +0900

    add Loading component

--- src/App.js
+++ src/App.js
@@ -22,7 +22,14 @@ const Image = (props) => {
   );
 };
 
+const Loading = () => {
+  return <p>Loading...</p>;
+};
+
 const Gallery = ({ urls }) => {
+  if (!urls) {
+    return <Loading />;
+  }
   return (
     <div className="columns is-vcentered is-multiline">
       {urls.map((url) => {
@@ -37,20 +44,7 @@ const Gallery = ({ urls }) => {
 };
 
 const Main = () => {
-  const urls = [
-    "https://images.dog.ceo/breeds/shiba/shiba-11.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-12.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-14.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-17.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-2.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-3i.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-4.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-5.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-6.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-7.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-8.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-9.jpg",
-  ];
+  const urls = null;
 
   return (
     <main>
